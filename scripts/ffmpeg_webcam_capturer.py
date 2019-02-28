d1a746ace2caa6e66b3eaf559fddcd0856a3bd26
WK CHAN <wkchan.linus@gmail.com>
2019-03-01 12:32:30 +1300

    Applied pylint suggestions
    1. Moved FFMPEGWebcamCapturer to ffmpeg_webcam_capturer
    2. added docstring
    3. changed method name and class name from camel case to snake case

--- scripts/ffmpeg_webcam_capturer.py
+++ scripts/ffmpeg_webcam_capturer.py
@@ -2,12 +2,17 @@ import ffmpeg
 import numpy as np
 from fire import Fire
 
-class FFMpegWebcamCapturer:
+
+class ffmpeg_webcam_capturer:
+    """ A class for utilising python-ffmpeg """
 
     def __init__(self):
         return
 
     def facetime(self):
+        """ facetime function is designed for capturing video from facetime
+        camera and store it into a video
+        """
         (
             ffmpeg
             .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30, s='1280x720')
@@ -15,20 +20,22 @@ class FFMpegWebcamCapturer:
             .overwrite_output()
             .run()
         )
+        return
 
-    def captureWebCam(self, out_filename: str, width, height):
+    def capture_web_cam(self, out_filename: str, width, height):
+        """ capture_web_cam function is design for capturing the video from facetime camera
+        and process it with a numpy function.
+        After processing it, the video will be saved in a file"""
         process1 = (
             ffmpeg
             .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30)
             .output('pipe:', format='rawvideo', pix_fmt='uyvy422')
             .run_async(pipe_stdout=True)
-            )
-
+        )
 
         process2 = (
             ffmpeg
-#            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='{}x{}'.format(width, height))
-            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='1280x720')
+            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='{}x{}'.format(width, height))
             .output(out_filename, format='mp4', pix_fmt='yuv420p')
             .overwrite_output()
             .run_async(pipe_stdin=True)

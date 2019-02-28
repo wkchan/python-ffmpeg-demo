69e1820f11602ac8f0743758bd66b9a2d6a17427
Cooomma <payton@cooomma.info>
2019-02-28 21:17:55 +0900

    modify syntax

--- scripts/FFMpegWebcamCapturer.py
+++ scripts/FFMpegWebcamCapturer.py
@@ -2,53 +2,36 @@ import ffmpeg
 import numpy as np
 from fire import Fire
 
+
 class FFMpegWebcamCapturer:
 
     def __init__(self):
-        return
+        pass
 
-    def facetime(self):
-        (
-            ffmpeg
-            .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30, s='1280x720')
-            .output('out.mp4', pix_fmt='yuv420p')
-            .overwrite_output()
+    def facetime():
+        ffmpeg \
+            .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30, s='1280x720') \
+            .output('out.mp4', pix_fmt='yuv420p') \
+            .overwrite_output() \
             .run()
-        )
 
-    def captureWebCam(self, out_filename: str, width, height):
-        process1 = (
-            ffmpeg
-            .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30)
-            .output('pipe:', format='rawvideo', pix_fmt='uyvy422')
+    def capture_web_cam(self, out_filename: str, width: int = 1280, height: int = 720):
+        process1 = ffmpeg \
+            .input('FaceTime', format='avfoundation', pix_fmt='uyvy422', framerate=30) \
+            .output('pipe:', format='rawvideo', pix_fmt='uyvy422') \
             .run_async(pipe_stdout=True)
-            )
 
-
-        process2 = (
-            ffmpeg
-#            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='{}x{}'.format(width, height))
-            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='1280x720')
-            .output(out_filename, format='mp4', pix_fmt='yuv420p')
-            .overwrite_output()
+        process2 = ffmpeg \
+            .input('pipe:', format='rawvideo', pix_fmt='uyvy422', s='{}x{}'.format(width, height)) \
+            .output(out_filename, format='mp4', pix_fmt='yuv420p') \
+            .overwrite_output() \
             .run_async(pipe_stdin=True)
-        )
 
-        while True:
+        in_bytes = process1.stdout.read(width * height)
+        while in_bytes:
+            in_frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width])
+            process2.stdin.write(in_frame.astype(np.uint8).tobytes())
             in_bytes = process1.stdout.read(width * height)
-            if not in_bytes:
-                break
-            in_frame = (
-                np
-                .frombuffer(in_bytes, np.uint8)
-                .reshape([height, width])
-            )
-            out_frame = in_frame
-            process2.stdin.write(
-                out_frame
-                .astype(np.uint8)
-                .tobytes()
-            )
         process2.stdin.close()
         process1.wait()
         process2.wait()

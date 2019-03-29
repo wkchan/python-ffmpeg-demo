e8a378f9e9023b2608c156f04288097eaa2d2040
wkchan.linus <wkchan.linus@gmail.com>
2019-03-29 16:51:33 +1300

    1. Modified test case file 2. Scan the test folder directly

--- scripts/tests/test_main.py
+++ scripts/tests/test_main.py
@@ -10,8 +10,8 @@ def test_ffprobe():
     assert(type(result) is dict)
 
 def test_ffmpeg():
-    in_filepath = os.path.abspath('./tests/resources/BigBuckBunny_320x180.mp4')
-    out_filepath = os.path.abspath('./tests/resources/BigBuckBunny_320x180_output.mp4')
+    in_filepath = os.path.abspath('./scripts/tests/resources/BigBuckBunny_320x180.mp4')
+    out_filepath = os.path.abspath('./scripts/tests/resources/BigBuckBunny_320x180_output.mp4')
     # ffmpeg.input(in_filepath).output(out_filepath, vcodec='copy', acodec='copy').overwrite_output().run()
     ffmpeg.input(in_filepath).output(out_filepath).overwrite_output().run()
     result = ffmpeg.probe(out_filepath)

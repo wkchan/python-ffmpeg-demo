a288c19a189283b644f12958084147cb437424c2
wkchan.linus <wkchan.linus@gmail.com>
2019-03-29 16:59:28 +1300

    Use acodec and vcode copy for speeding up the test process

--- scripts/tests/test_main.py
+++ scripts/tests/test_main.py
@@ -11,9 +11,9 @@ def test_ffprobe():
 
 def test_ffmpeg():
     in_filepath = os.path.abspath('./scripts/tests/resources/BigBuckBunny_320x180.mp4')
-    out_filepath = os.path.abspath('./scripts/tests/resources/BigBuckBunny_320x180_output.mp4')
+    out_filepath = os.path.abspath('./scripts/tests/resources/BigBuckBunny_320x180_output.avi')
     # ffmpeg.input(in_filepath).output(out_filepath, vcodec='copy', acodec='copy').overwrite_output().run()
-    ffmpeg.input(in_filepath).output(out_filepath).overwrite_output().run()
+    ffmpeg.input(in_filepath).output(out_filepath, acodec='copy', vcodec='copy').overwrite_output().run()
     result = ffmpeg.probe(out_filepath)
     assert(type(result) is dict)
     os.remove(out_filepath)
